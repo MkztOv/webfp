17a955ff682454d81339eaa86ae9de1dbd8d4a0d
ataka <atakajovan@gmail.com>
2019-07-03 15:49:39 +0700

    menambah detailpegawai,nonaktif,habis

--- resources/views/pegawai/detail_pegawai.blade.php
+++ resources/views/pegawai/detail_pegawai.blade.php
@@ -15,73 +15,88 @@
     </ol>
 </section>
 <section>
-  <div class="container">
-    <div class="card">
-      <div class="card-body">
-        <div style="overflow-x:auto;">
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th>Pilih</th>
-              <th>PIN</th>
-              <th>NIP</th>
-              <th>Nama</th>
-              <th>Jadwal Kerja</th>
-              <th>Tgl. Mulai Jadwal</th>
-              <th>Tempat Lahir</th>
-              <th>Tanggal Lahir</th>
-              <th>Jabatan</th>
-              <th>Departemen</th>
-              <th>Kantor</th>
-              <th>Password</th>
-              <th>RFID</th>
-              <th>No. Telp</th>
-              <th>Privilage</th>
-              <th>Status Pegawai</th>
-              <th>FP ZK</th>
-              <th>FP Neo</th>
-              <th>FP Revo</th>
-              <th>FP Livo</th>
-              <th>FP UareU</th>
-              <th>Face</th>
-              <th>Tgl. Masuk Kerja</th>
-              <th>Tgl. Akhir Kerja</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+
+<body>
+ 
+	<div class="container">
+		<div class="card">
+			<div class="card-body">
+				
+ 
+				<h3>Detail Pegawai</h3>
+				<br/>
+                <div>
+                <a class="btn btn-success btn-sm" href="/#">Foto Pegawai</a>
+                <a href="http://">clear filter data</a>
+                <a class="btn btn-success btn-sm" href="/#">Export</a>
+                <a class="btn btn-success btn-sm" href="/#">Cetak</a>
+                </div>
+                
+                <br/>
+                <br/>
+                <div style="overflow-x:auto;">
+                <table class="table table-bordered">
+				<tr>
+                        <th style="text-align:center" colspan ="14" >Detail Pegawai</th>
+                        <th style="text-align:center" colspan="3">Nama</th>
+                </tr>
+                <tr>
+                    <th>PIN</th>
+                    <th>NIP</th>
+                    <th>Nama</th>
+                    <th>Alias</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Pend. Terakhir</th>
+                    <th>Golongan Darah</th>
+                    <th>Status Pernikahan</th>
+                    <th>Agama</th>
+                    <th>Jumlah Anak</th>
+                    <th>Alamat</th>
+                    <th>Nama Bank</th>
+                    <th>Nama Rekening</th>
+                    <th>No. Rekening</th>
+                    <th>Nama</th>
+                    <th>No. Telpon</th>
+                    <th>Hubungan</th> 
+                        
+                </tr>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                    
+					
+				</table>
+                </div>
+				
+ 
+				<br/>
+				
+				<br/>
+ 
+				
+			</div>
+		</div>
+	</div>
+ 
+ 
+</body>
+  </section>
 
 
 @endsection
