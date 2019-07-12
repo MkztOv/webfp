ed954c9bb4879e0ea45312cc97cfe7375311e37f
MkztOv <Yuda@Gre365ti.onmicrosoft.com>
2019-07-12 16:17:35 +0700

    perbaikan view tambah pegawai

--- resources/views/pegawai/foldertambahpegawai/tambah.blade.php
+++ resources/views/pegawai/foldertambahpegawai/tambah.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="content-header">
-  <h1></h1> <small>Tambah pegawai</small>
+  <h1>Tambah Pegawai Baru</h1>
     <ol class="breadcrumb">
         <li>
           <a href="{{url('/home')}}"><i class="fa fa-home"></i>Home</a>
@@ -19,45 +19,54 @@
     </ol>
 </section>
 
-<section>
+<section class="content">
 <body>
 
 	<div class="container-fluid">
 		<div class="card">
 			<div class="card-body">
-				<h3>Tambah Pegawai Baru</h3>
+        <a class="btn btn-info btn-sm" href="{{url('/aktif')}}">Kembali</a>
+        <br/>
+        <br/>
+        <div class="col-md-6">
+          <div class="box">
+            <div class="box-body">
+              <form action="" method="post">
 
-    <a class="btn btn-info btn-sm" href="{{url('/aktif')}}">Kembali</a>
-    <br/>
-    <br/>
+                  {{ csrf_field()}}
 
-    <form action="" method="post">
+              <div class="form-group">
+              <label for="name">PIN</label> <br/>
+              <input type="text" name="pin" required="requered" placeholder="1234"> <br/>
+              </div>
 
-        {{ csrf_field()}}
+              <div class="form-group">
+              <label for="name">NIP</label> <br/>
+              <input type="text" name="pin" required="requered" placeholder="4321"> <br/>
+              </div>
 
-    <div class="form-group">
-    <label for="name">PIN</label> <br/>
-    <input type="text" name="pin" required="requered"> <br/>
-    </div>
+              <div class="form-group">
+              <label for="name">Nama</label> <br/>
+              <input type="text" name="nama" required="requered" placeholder="Nama Lengkap"> <br/>
+              </div>
 
-    <div class="form-group">
-    <label for="name">Nama</label> <br/>
-    <input type="text" name="nama" required="requered"> <br/>
-    </div>
+              <div class="form-group">
+              <label for="name">Jadwal Kerja</label> <br/>
+              <input type="text" name="jadwal" required="requered" placeholder="Jadwal Kerja"> <br/>
+              </div>
 
-    <div class="form-group">
-    <label for="name">Jadwal Kerja</label> <br/>
-    <input type="text" name="jadwal" required="requered"> <br/>
-    </div>
+              <div class="form-group">
+              <label for="name">Tgl. Mulai Jadwal Kerja</label> <br/>
+              <input type="text" name="tglmulai" required="requered" placeholder="Tanggal Mulai jadwal"> <br/>
+              </div>
 
-    <div class="form-group">
-    <label for="name">Tgl. Mulai Jadwal</label> <br/>
-    <input type="text" name="tglmulai" required="requered"> <br/>
-    </div>
+                  <input class="btn btn-success btn-sm" type="submit" value="Simpan Data">
 
-        <input class="btn btn-success btn-sm" type="submit" value="Simpan Data">
+              </form>
+            </div>
 
-    </form>
+          </div>
+        </div>
 			</div>
 		</div>
 	</div>
