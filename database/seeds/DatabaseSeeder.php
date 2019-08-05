e5f239467d11851c37030281c9d07a1344fbabe7
MkztOv <Yuda@Gre365ti.onmicrosoft.com>
2019-08-05 09:35:55 +0700

    Seeder default user, role admin, run seeder first then login

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+    		'name' => 'SiAdmin',
+        'role' => 'admin',
+    		'email' => 'SiAdmin@SiAdmin.com',
+    		'password' => bcrypt('123456'), //Hash default laravel
+    		'created_at' => date('Y-m-d H:i:s'),
+    		'updated_at' => date('Y-m-d H:i:s'),
+    	]);
     }
 }
